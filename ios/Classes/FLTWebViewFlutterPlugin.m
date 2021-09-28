bc66cfc8059c51548d1d9434f637a7a486963d89
LoquatZ <loquatz@qq.com>
2021-09-28 15:49:23 +0800

    [add] iOS URL 拦截.

--- ios/Classes/FLTWebViewFlutterPlugin.m
+++ ios/Classes/FLTWebViewFlutterPlugin.m
@@ -5,14 +5,20 @@
 #import "FLTWebViewFlutterPlugin.h"
 #import "FLTCookieManager.h"
 #import "FlutterWebView.h"
+#import "FlutterNSURLProtocol.h"
+#import "NSURLProtocol+WKWebView.h"
 
 @implementation FLTWebViewFlutterPlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FLTWebViewFactory* webviewFactory =
-      [[FLTWebViewFactory alloc] initWithMessenger:registrar.messenger];
-  [registrar registerViewFactory:webviewFactory withId:@"plugins.flutter.io/webview"];
-  [FLTCookieManager registerWithRegistrar:registrar];
+    FLTWebViewFactory* webviewFactory =
+    [[FLTWebViewFactory alloc] initWithMessenger:registrar.messenger];
+    [registrar registerViewFactory:webviewFactory withId:@"plugins.flutter.io/webview"];
+    [FLTCookieManager registerWithRegistrar:registrar];
+    //注册拦截所有请求
+    [NSURLProtocol registerClass:[FlutterNSURLProtocol class]];
+    [NSURLProtocol wk_registerScheme:@"http"];
+    [NSURLProtocol wk_registerScheme:@"https"];
 }
 
 @end
---
+++ ios/Classes/FlutterNSURLProtocol.h
@@ -0,0 +1,15 @@
+//
+//  FlutterNSURLProtocol.h
+//  Pods-Runner
+//
+//  Created by yanxx on 2019/11/18.
+//
+
+#import <Foundation/Foundation.h>
+#import <Flutter/Flutter.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlutterNSURLProtocol : NSURLProtocol
+@end
+
+NS_ASSUME_NONNULL_END
